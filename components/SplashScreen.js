1b2a5ed1b94cb2a94846ddd7cb620a840464571d
Quinlann <hegs_hta@hotmail.com>
2023-03-14 15:05:28 +0100

    Added splash screen component. Only HTML markup.

---
+++ components/SplashScreen.js
@@ -0,0 +1,17 @@
+export default {
+    template:
+    /* html */
+    `
+    <div id="splashscreen-container">
+
+        <div id="splashscreen">
+            <h1 id="splash-header">PEARL PLATE PYTHON</h1>
+            <div id="splash-controls-container">
+                <input type="text" id="username-input" value="Player 1" placeholder="Enter your username" />
+                <button id="splash-start-game-btn">START</button>
+                <button id="splash-highscore-btn">HIGH SCORE</button>
+            </div>
+        </div>
+    </div>
+    `
+}
--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 import Canvas from './components/Canvas.js';
 import SnakeHead from './components/SnakeHead.js';
+import SplashScreen from './components/SplashScreen.js';
 
 const app = Vue.createApp({
     template: 
     /* html */
     `
     <Canvas/>
+    <SplashScreen/>
     `
 });
 
 app.component('Canvas', Canvas);
 app.component('SnakeHead', SnakeHead);
+app.component('SplashScreen', SplashScreen);
 
 app.mount('#app');
