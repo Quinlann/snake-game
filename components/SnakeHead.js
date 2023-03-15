7c3b04bf3f914db80339aeeb35890f2fd5f4a796
Quinlann <hegs_hta@hotmail.com>
2023-03-15 15:11:11 +0100

    Added keyhandler to SnakeHead

--- components/SnakeHead.js
+++ components/SnakeHead.js
@@ -1,9 +1,38 @@
 export default {
     props: ['cellSize'],
     data() {
-        return {}
+        return {
+            controlsStatus: 'normal'
+        }
     },
-    methods: {},
+    methods: {
+        keyHandler(event) {
+            if (event.code === 'KeyW' || event.code === 'ArrowUp') {
+                if (this.controlsStatus === 'normal') this.pressUp();
+            } else if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
+                if (this.controlsStatus === 'normal') this.pressLeft();
+            } else if (event.code === 'KeyS' || event.code === 'ArrowDown') {
+                if (this.controlsStatus === 'normal') this.pressDown();
+            } else if (event.code === 'KeyD' || event.code === 'ArrowRight') {
+                if (this.controlsStatus === 'normal') this.pressRight();
+            }
+        },
+        pressUp() {
+            console.table('UP');
+        },
+        pressLeft() {
+            console.table('LEFT');
+        },
+        pressDown() {
+            console.table('DOWN');
+        },
+        pressRight() {
+            console.table('RIGHT');
+        },
+    },
+    beforeMount() {
+        document.addEventListener('keydown', this.keyHandler);
+    },
     mounted() {},
     template:/* html */
     `
